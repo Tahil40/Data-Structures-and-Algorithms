8f4fb39a433af9e995d4598d74e9b6a6db4b0bff
Tahil40 <www.mohd.tahilnazir9216@gmail.com>
2026-09-02 15:47:24 +0530

    configured_DecimalToBinary.java

--- DSA_Java/code/Functions/BinaryToDecimal.java
+++ DSA_Java/code/Functions/BinaryToDecimal.java
@@ -1,20 +1,25 @@
 import java.util.*;
 
 public class BinaryToDecimal {
+    public static void ConvertToDecimal(int number){
+        int StartingNumber = number;
+        int decimalNumber = 0; 
+        int pow = 0; 
+
+        while(number!=0){
+            int last_number = number % 10; 
+            decimalNumber = decimalNumber + (int) Math.pow(2, pow) * last_number;
+            
+            pow++; 
+            number = number / 10; 
+        };
+        System.out.println("The Decimal Number of {"+ StartingNumber +"} is: "+ decimalNumber);
+    };
+
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in); 
         System.out.print("Enter the Number; "); 
         int user_input = scanner.nextInt(); 
-         
-        int decimalNumber = 0; 
-        int pow = 0; 
-        while(user_input!=0){
-            int last_number = user_input % 10; 
-            decimalNumber = decimalNumber + (int) Math.pow(2, pow) * last_number;
-            
-            pow++; 
-            user_input = user_input / 10; 
-        };
-        System.out.println("The Decimal Number is: "+ decimalNumber);
+        ConvertToDecimal(user_input);
     }
 }
---
+++ DSA_Java/code/Functions/DecimalToBinary.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class DecimalToBinary {
+    public static void ConvertToBinary(int number){
+        int StartingNumber = number;
+        int power = 0; 
+        int binaryNumber = 0; 
+        
+        while(number!=0){
+            int reminder = number % 2;
+            binaryNumber = binaryNumber + (reminder * (int) Math.pow(10, power));
+            System.out.println("Binary Number is; "+ binaryNumber);
+            power++; 
+            number = number / 2; 
+        }; 
+        System.out.println("The Binary Number of {"+ StartingNumber +"} is: "+ binaryNumber);
+    };
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("Enter The Number; ");
+        int input_number = scanner.nextInt();
+        ConvertToBinary(input_number);
+    };
+};
